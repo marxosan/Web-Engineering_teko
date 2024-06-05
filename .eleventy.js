d5d72022cc77eae8abb29b7d7e1bdfb70c9d9d33
marxosan <marco.hofer99@hotmail.com>
2024-06-05 11:36:16 +0200

    final

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy({ "src/css/styles.css": "styles.css" });
   eleventyConfig.addPassthroughCopy("src/img");
 
   return {
